54563d03bfd9a95edd9dec727265c7b5b52bb765
t0stiman <18124323+t0stiman@users.noreply.github.com>
2021-04-21 18:55:43 +0200

    [FreeGamesBridge] fix formatting

--- bridges/FreeGamesBridge.php
+++ bridges/FreeGamesBridge.php
@@ -1,37 +1,39 @@
 <?php
-class FreeGamesBridge extends BridgeAbstract {
-//todo rename bridge
-	const NAME = 'Free EGS Games Bridge';
-	const URI = 'https://ep.reddit.com/r/FreeGameFindings/new';
-	const DESCRIPTION = 'Free EGS Games';
-	const MAINTAINER = 't0stiman';
+class FreeGamesBridge extends BridgeAbstract
+{
+    //todo rename bridge
+    const NAME = 'Free EGS Games Bridge';
+    const URI = 'https://ep.reddit.com/r/FreeGameFindings/new';
+    const DESCRIPTION = 'Free Epic Game Store games';
+    const MAINTAINER = 't0stiman';
 
-	public function collectData() {
-		$page = getSimpleHTMLDOMCached(self::URI, self::CACHE_TIMEOUT)
-			or returnServerError('could not retrieve page');
+    public function collectData()
+    {
+        $page = getSimpleHTMLDOMCached(self::URI, self::CACHE_TIMEOUT)
+        or returnServerError('could not retrieve page');
 
-		//for each post
-		foreach($page->find('div.link a.title') as $post) {
-			$item = array();
+        //for each post
+        foreach($page->find('div.link a.title') as $post) {
+            $item = array();
 
-			$uri = $post->getAttribute('href');
-			//filter out the ads and stuff
-			if($uri == '' || !stripos($uri, 'epicgames.com')){
-				continue;
-			}
-			$title = $post->innertext;
-			//only games pls
-			if(!stripos($title, '(game)')){
-				continue;
-			}
+            $uri = $post->getAttribute('href');
+            //filter out the ads and stuff
+            if($uri == '' || !stripos($uri, 'epicgames.com')) {
+                continue;
+            }
+            $title = $post->innertext;
+            //only games pls
+            if(!stripos($title, '(game)')) {
+                continue;
+            }
 
-			$item['uri'] = $uri;
-			$item['title'] = $title;
-			$item['content'] = '<p><a href="' . $uri . '"> Go to giveaway.</a></p>';
-			$item['author'] = 'Epic Games Store';
+            $item['uri'] = $uri;
+            $item['title'] = $title;
+            $item['content'] = '<p><a href="' . $uri . '"> Go to giveaway.</a></p>';
+            $item['author'] = 'Epic Games Store';
 
-			//add the item to the list
-			$this->items[] = $item;
-		}		
-	}
+            //add the item to the list
+            $this->items[] = $item;
+        }        
+    }
 }
